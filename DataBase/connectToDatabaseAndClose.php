d5866ff23cb7496311199ca83f283a6ab739c4a1
Miel <mielfidder@gmail.com>
2020-03-19 10:20:06 +0100

    Set up van eerste pagina's

---
+++ DataBase/connectToDatabaseAndClose.php
@@ -0,0 +1,26 @@
+<?php
+//connectToDatabaseAndClose.include
+$host = "localhost";
+$databaseName = "HappyBrides";
+$connectionString = "mysql:host=$host;dbname=$databaseName";
+$username = "student";     //root is default in most cases
+$password = "student";     //root is default in most cases
+
+$conn = null;
+
+try {
+    $conn = new PDO($connectionString, $username, $password);
+
+    //enables exception mode, exception is throw when an error occurs
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //echo "Connected successful";
+} catch (PDOException $ex) {
+    echo "PDOException:  $ex";
+} finally {
+    if($conn != null) {
+        //$conn->close();
+        //$conn = null;
+    }
+}
+?>
---
+++ include/NavBar.php
@@ -0,0 +1,21 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary navbar-fixed-top">
+        <a class="navbar-brand" href="index.php"><img src="../pics/NHL-Stenden-Logo.png"></a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Kom ik in aanmerking?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Log in</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
