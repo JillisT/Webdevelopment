05922b1b78c2e9a6a10070c3a701e6fc0153fc36
Miel <mielfidder@gmail.com>
2020-03-20 14:19:03 +0100

    login pagina link gefixt

--- include/NavBar.php
+++ include/NavBar.php
@@ -1,21 +1,37 @@
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary navbar-fixed-top">
-        <a class="navbar-brand" href="index.php"><img src="../pics/NHL-Stenden-Logo.png"></a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <!-- row start -->
+<div class="row">
+    <!-- white space rechts -->
+    <div class="col-lg-2">
+            <a class="navbar-brand" href="https://www.qries.com/">
+                <img alt="Qries" src="pics/logo.png"
+                     width=150">
+            </a>
+    </div>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">FAQ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Kom ik in aanmerking?</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Log in</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <!-- body start -->
+    <div class="col-lg-10">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary navbar-fixed-top">
+
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Kom ik in aanmerking?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Log in</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,102 +1,95 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-  <head>
-      <?php
-      require "include/stylesheets.php";
-      ?>
-      <title>Home</title>
-  </head>
+<head>
+    <?php
+    require "include/stylesheets.php";
+    ?>
+    <title>Home</title>
+</head>
 
-  <body>
-    <!-- row start -->
-    <div class="row">
-        <!-- white space rechts -->
-      <div class="col-lg-2"></div>
-  
-      <!-- body start -->
-      <div class="col-lg-8">
-  
+<body>
+<!-- row start -->
+<div class="row">
+    <!-- white space rechts -->
+    <div class="col-lg-2"></div>
+
+    <!-- body start -->
+    <div class="col-lg-8">
+
         <!-- container body start -->
         <div class="container-fluid">
 
-        <?php
-        include "include/carousel.php";
-        require "include/NavBar.php";
-        ?>
+            <?php
+            require "include/NavBar.php";
+            ?>
 
-          <!--item row 1 begin-->
-          <div class="row mt-4">
+            <!--item row 1 begin-->
+            <div class="row mt-4">
 
-            <div class="col-sm-12 col-md-4">
-              <div class="card mb-4">
-                <div class="card-body text-center">
-                  <h5 class="card-title">Lijst voor het bruidspaar</h5>
-                  <p class="card-text">
-                    Bent u op zoek naar een handige manier om uw wenslijst bij te houden? Maak een account aan en deel de code met uw gasten.
-                  </p>
-                  <a href="registerBP.php" class="card-link">Maak een account</a>
+                <div class="col-sm-12 col-md-12">
+                    <div class="card mb-4">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Kom ik in aanmerking voor het profileringsfonds?</h5>
+                            <a href="login.php" class="card-link">Heb je al een account? Log hier in.</a>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-            <div class="col">
-              <div class="card">
-                <div class="card-body text-center">
-                  <h5 class="card-title">Ik ben een gast</h5>
-                  <p class="card-text">
-                    Kijk naar de wensen van het bruidspaar, kies welk cadeau u gaat kopen en reserveer dit idee. Maak hier een account om bij de lijst te komen.
-                  </p>
-                  <a href="registerG.php.php" class="card-link">Wenslijst</a>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card">
-                <div class="card-body text-center">
-                  <h5 class="card-title">Contact</h5>
-                  <p class="card-text">
-                    Heeft u vragen? bruid, bruidegom of gast; u kunt hier terecht.
-                  </p>
-                  <a href="#" class="card-link">contact</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!--item row 1 end-->
+            <!--item row 1 end-->
 
-          <!-- qoute start -->
-          <div class="row mt-sm-4 mt-0">
-            <div class="col-sm-12 col-md-8 text-sm-center text-md-left">
-              <blockquote class="blockquote">
-                <p class="mb-0">“We’re all a little weird. And life is a little weird. And when we find someone whose weirdness is compatible with ours, we join up with them and fall into mutually satisfying weirdness–and call it love – true love.”</p>
-                <p class="blockquote-footer">Robert Fulghum <cite title="Source Title"> True Love</cite></p>
-              </blockquote>
-            </div>
-          </div>
-          <!-- qoute end -->
+            <!-- bottom text begin -->
+            <div class="row mt-4">
+                <div class="col-sm-12 col-md-6">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas nec urna vitae
+                        efficitur. Aliquam erat volutpat. Phasellus lobortis dolor eros, vitae tincidunt sapien tempor
+                        sit amet. Vestibulum venenatis hendrerit posuere. Sed id congue odio. Ut vitae molestie tellus,
+                        sed pretium lorem. Nunc at commodo erat. Curabitur quis consequat sem, nec porttitor est.
+                        Vestibulum hendrerit vulputate lorem, eu tempus dui porttitor sed. Morbi ac neque gravida,
+                        ultricies velit ac, auctor justo. Curabitur tellus nibh, scelerisque sed tortor sit amet,
+                        viverra aliquet velit. Nulla viverra sed turpis eget iaculis. Praesent sit amet nulla et nisl
+                        hendrerit efficitur at ut purus. Vivamus ac malesuada eros.
 
-          <!-- footer start -->
-          <footer class="footer">
-            <div class="container">
-              <p>Made by: Miel Fidder</p>
+                        Duis sed libero urna. Phasellus egestas lacus massa, vel malesuada diam aliquam ac. Morbi eu
+                        diam vulputate, iaculis leo quis, pretium nibh. Aliquam ac massa et ipsum mattis hendrerit.
+                        Vivamus at faucibus nunc. Nam hendrerit nibh ut dui congue, sit amet finibus nunc luctus.
+                        Vivamus sit amet massa ac velit luctus blandit eget eget magna. Vestibulum tincidunt iaculis
+                        blandit.
+                    </p>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <p>
+                        Nullam rhoncus eget lacus vel rhoncus. Aliquam rutrum imperdiet porta. Vivamus lobortis
+                        sapien eros, ut posuere ante posuere sit amet. Pellentesque fringilla orci vel iaculis
+                        hendrerit. Ut in est vehicula, placerat tellus vitae, fringilla ipsum. Proin fermentum, nunc
+                        non consequat porta, massa ex fermentum dui, in lacinia augue lectus eu elit. Nam non leo
+                        volutpat, pellentesque augue a, dictum tellus. Interdum et malesuada fames ac ante ipsum
+                        primis in faucibus. Proin consectetur varius dictum. Ut dictum interdum vestibulum. Cras at
+                        erat vitae mi elementum blandit ut non velit. Proin luctus pellentesque purus aliquet
+                        ullamcorper.
+
+                        Aliquam volutpat felis eu dolor varius scelerisque. Praesent scelerisque pretium turpis at
+                        pharetra.
+                        Fusce in laoreet sem. Aliquam id nisl sagittis, euismod turpis vel, pellentesque lacus. Duis
+                        justo
+                        quam, tristique ac ornare eget, sollicitudin at magna. Sed ex dolor, porttitor eget neque vel,
+                        lobortis fringilla quam. Quisque erat nisi, tempor id nisl ac, luctus convallis diam.
+
+                    </p>
+                </div>
             </div>
-          </footer>
-          <!-- footer end -->
-      
-          </div>
-          <!-- container body end -->
-  
+            <!-- bottom text end -->
+
         </div>
-        <!-- body end -->
-  
-      <!-- white space links -->
-      <div class="col-lg-2"></div>
-      </div>
-      <!-- row end -->
 
-      <!-- scripts -->
-      <script src="js/jquery-3.4.1.js"></script>
-      <script src="/js/jquery.min.js"></script>
-      <script src="/js/popper.min.js"></script>
-      <script src="/js/bootstrap.min.js"></script>
-    </body>
-  </html>
+
+        <!-- container body end -->
+
+        <!-- white space links -->
+        <div class="col-lg-2"></div>
+    </div>
+</div>
+<!-- row end -->
+
+</body>
+</html>
