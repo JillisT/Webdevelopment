f9e973837978ce024bca2fd0d33bca365635a5b1
Miel <mielfidder@gmail.com>
2020-03-23 12:34:13 +0100

    Dashboard afgemaakt

--- dashboard.php
+++ dashboard.php
@@ -24,16 +24,22 @@
             ?>
             <h3 class="mt-5">Jan de Jong</h3>
             <div class="col-sm-12 col-md-12">
-                <div class="card" style="height: 600px;">
+                <div class="card" style="height: 400px;">
                     <div class="row">
                         <div class="col-sm-2 col-md-6">
-
+                            <br>
+                            <p>Onder de brug 24</p>
+                            <p>1234 DE, Leeuwarden</p>
+                            <br>
+                            <p>Studie: &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; HBO-ICT</p> <!-- Misschien even kijken hoe dit netter kan. -->
+                            <p>studienummer: &emsp;&emsp;&ensp; 123456789</p>
+                            <p>Aanvraagnummer: &emsp;-</p>
                         </div>
-                        <div class="col-sm-2 col-md-6">
-                            <div class="d-flex flex-row-reverse">
-                                <button class="">Nieuwe aanvraag</button>
-                                <div class="align-self-end">Nog geen aanvraag gedaan.</div>
-                            </div>
+                        <div class="col-sm-2 col-md-6 d-flex flex-row-reverse">
+                            <form method="post">
+                                <button class="btn btn-primary" style="margin-top: 330px; margin-right: 30px; margin-left: 10px;" type="submit">Nieuwe aanvraag</button>
+                            </form>
+                            <div class="align-self-end">Nog geen aanvraag gedaan.</div>
                         </div>
                     </div>
                 </div>
