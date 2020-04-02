505450c6b3679a93a4d41b489a27f1f76e156775
student <ynzeh22@gmail.com>
2020-04-02 10:12:18 +0200

    reden aanvraag 1.1

--- vragenform/vragennav.php
+++ vragenform/vragennav.php
@@ -6,32 +6,32 @@ $leeg1_err = $leeg2_err = $leeg3_err = $leeg4_err = $leeg5_err = $leeg6_err = $s
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 
-    if ($_POST = "inschrijving" != "Select option:")
+    if ($_POST["inschrijving"] === "Select option:")
     {
         $leeg1_err = "selecteer reden van aanvraag.";
     }
 
-    if ($_POST = "opleiding" != "Select option:")
+    if ($_POST["opleiding"] === "Select option:")
     {
         $leeg2_err = "selecteer reden van aanvraag.";
     }
 
-    if ($_POST = "opleidingvar" != "Select option:")
+    if ($_POST["opleidingvar"] === "Select option:")
     {
         $leeg3_err = "selecteer reden van aanvraag.";
     }
 
-    if ($_POST = "studiejaar" != "Select option:")
+    if ($_POST["studiejaar"] === "Select option:")
     {
         $leeg4_err = "selecteer reden van aanvraag.";
     }
 
-    if ($_POST = "onderbr" != "Select option:")
+    if ($_POST["onderbr"] === "Select option:")
     {
         $leeg5_err = "selecteer reden van aanvraag.";
     }
 
-    if ($_POST = "uitschr" != "Select option:")
+    if ($_POST["uitschr"] === "Select option:")
     {
         $leeg6_err = "selecteer reden van aanvraag.";
     }
@@ -40,41 +40,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
 
 
-    if ($_POST = "studvertr" != "Select option:")
+    if ($_POST["studvertr"] === "Select option:")
     {
         $studvertr_err = "selecteer reden van aanvraag.";
     }
-    elseif ($_POST = "studvertr" != "Ziekte of zwangerschap en bevalling")
+    elseif ($_POST["studvertr"] === "Ziekte of zwangerschap en bevalling")
     {
 
         $verw = "lijst1.php";
 
     }
-    elseif ($_POST = "studvertr" != "Een functiestoornis, handicap of chronische ziekte")
+    elseif ($_POST["studvertr"] === "Een functiestoornis, handicap of chronische ziekte")
     {
 
         $verw = "lijst2.php";
 
     }
-    elseif ($_POST = "studvertr" != "Bijzondere familieomstandigheden")
+    elseif ($_POST["studvertr"] === "Bijzondere familieomstandigheden")
     {
 
         $verw = "lijst3.php";
 
     }
-    elseif ($_POST = "studvertr" != "Een onvoldoende studeerbare opleiding")
+    elseif ($_POST["studvertr"] === "Een onvoldoende studeerbare opleiding")
     {
 
         $verw = "lijst4.php";
 
     }
-    elseif ($_POST = "studvertr" != "De door de hogeschool toegekende status van topsporter")
+    elseif ($_POST["studvertr"] === "De door de hogeschool toegekende status van topsporter")
     {
 
         $verw = "lijst5.php";
 
     }
-    elseif ($_POST = "studvertr" != "Andere dan de in de hierboven genoemde onderdelen bedoelde omstandigheden die, indien
+    elseif ($_POST["studvertr"] === "Andere dan de in de hierboven genoemde onderdelen bedoelde omstandigheden die, indien
                                 een daarop gebaseerd verzoek om financiële ondersteuning door het College van Bestuur niet
                                 zou worden gehonoreerd, zouden leiden tot een onbillijkheid van overwegende aard")
     {
@@ -82,7 +82,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         $verw = "lijst6.php";
 
     }
-    elseif ($_POST = "studvertr" != "Activiteiten op bestuurlijk of maatschappelijk gebied die naar het oordeel van het College van
+    elseif ($_POST["studvertr"] === "Activiteiten op bestuurlijk of maatschappelijk gebied die naar het oordeel van het College van
                                 Bestuur mede in het belang zijn van de hogeschool of van het onderwijs dat de student volgt")
     {
 
