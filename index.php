0bb688184091230d58c53a515c6bceee9ce37776
student <student>
2020-04-14 14:07:56 +0200

    placeholders opgevuld en forms consistent gemaakt.

--- index.php
+++ index.php
@@ -45,40 +45,32 @@
             <div class="row mt-4">
                 <div class="col-sm-12 col-md-6">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas nec urna vitae
-                        efficitur. Aliquam erat volutpat. Phasellus lobortis dolor eros, vitae tincidunt sapien tempor
-                        sit amet. Vestibulum venenatis hendrerit posuere. Sed id congue odio. Ut vitae molestie tellus,
-                        sed pretium lorem. Nunc at commodo erat. Curabitur quis consequat sem, nec porttitor est.
-                        Vestibulum hendrerit vulputate lorem, eu tempus dui porttitor sed. Morbi ac neque gravida,
-                        ultricies velit ac, auctor justo. Curabitur tellus nibh, scelerisque sed tortor sit amet,
-                        viverra aliquet velit. Nulla viverra sed turpis eget iaculis. Praesent sit amet nulla et nisl
-                        hendrerit efficitur at ut purus. Vivamus ac malesuada eros.
 
-                        Duis sed libero urna. Phasellus egestas lacus massa, vel malesuada diam aliquam ac. Morbi eu
-                        diam vulputate, iaculis leo quis, pretium nibh. Aliquam ac massa et ipsum mattis hendrerit.
-                        Vivamus at faucibus nunc. Nam hendrerit nibh ut dui congue, sit amet finibus nunc luctus.
-                        Vivamus sit amet massa ac velit luctus blandit eget eget magna. Vestibulum tincidunt iaculis
-                        blandit.
+                    <h5><br><b>Wat is het profileringsfonds?</b></br></h5>
+                    <br>Het profileringsfonds is een fonds waar je een beroep op kunt doen als je door bijzondere omstandigheden niet binnen de reguliere studieduur kunt afstuderen.</br>
+
+
+                    <h5><br><b>Kom ik in aanmerking voor het profileringsfonds? Dit zijn de voorwaarden waaraan je moet voldoen voor het Profileringsfonds.</b></br></h5>
+
+
+                        <br>- Je hebt collegegeld betaald voor de opleiding.</br>
+                        <br>- Je ontvangt studiefinanciering.</br>
+                        <br>- Je hebt studievertraging opgelopen door bijzondere omstandigheden.</br>
+
+                        <br>Als je aan de bovenstaande 3 voorwaarden voldoet dan kom je in aanmerking voor het profileringsfonds.</br>
                     </p>
                 </div>
                 <div class="col-sm-12 col-md-6">
                     <p>
-                        Nullam rhoncus eget lacus vel rhoncus. Aliquam rutrum imperdiet porta. Vivamus lobortis
-                        sapien eros, ut posuere ante posuere sit amet. Pellentesque fringilla orci vel iaculis
-                        hendrerit. Ut in est vehicula, placerat tellus vitae, fringilla ipsum. Proin fermentum, nunc
-                        non consequat porta, massa ex fermentum dui, in lacinia augue lectus eu elit. Nam non leo
-                        volutpat, pellentesque augue a, dictum tellus. Interdum et malesuada fames ac ante ipsum
-                        primis in faucibus. Proin consectetur varius dictum. Ut dictum interdum vestibulum. Cras at
-                        erat vitae mi elementum blandit ut non velit. Proin luctus pellentesque purus aliquet
-                        ullamcorper.
+                    <h5><br><b>Hoe vraag ik steun van het profileringsfonds aan?</b></br></h5>
+                        <br>Neemt contact op met je decaan. Deze zal je helpen om een account aan te maken. Je aanvraag vul je volledig in op dit digitale aanvraagformulier. Ook je decaan en
+                    studiebegeleider/coach vullen dit digitaal in. </br>
 
-                        Aliquam volutpat felis eu dolor varius scelerisque. Praesent scelerisque pretium turpis at
-                        pharetra.
-                        Fusce in laoreet sem. Aliquam id nisl sagittis, euismod turpis vel, pellentesque lacus. Duis
-                        justo
-                        quam, tristique ac ornare eget, sollicitudin at magna. Sed ex dolor, porttitor eget neque vel,
-                        lobortis fringilla quam. Quisque erat nisi, tempor id nisl ac, luctus convallis diam.
-
+                    <h5><br><b>Hoe en wanneer wordt mijn aanvraag behandeld?</b></br></h5>
+                    <br><b>Alleen volledige aanvragen worden in behandeling genomen.</b> Is je aanvraag niet compleet, dan wordt je hiervan op de hoogte gesteld en moet je de aanvraag
+                    aanvullen en opnieuw indienen. Je aanvraag zal worden behandeld in de vergadering. De aanvraag moet 2 weken voor de vergadering worden ingediend. Mocht een aanvraag later
+                    worden ingediend dan zal je aanvraag tijdens de eerstvolgende vergadering in behandeling worden
+                    genomen. Voor vergaderdata, neem contact op met profileringsfonds@nhlstenden.com.</br>
                     </p>
                 </div>
             </div>
